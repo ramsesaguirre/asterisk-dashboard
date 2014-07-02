5d3377777bff2d1d3ae6d38f8ae90a72ae7c622c
Sirachat <sirachat.sir@gmail.com>
2014-07-02 15:35:22 +0700

    Version 2 : Update example files (for crontab)
    อัพเดทไฟล์เกี่ยวข้องกับที่ต้องใช้ใน crontab นะจ๊ะ

---
+++ example_files(crontab).sh
@@ -0,0 +1,27 @@
+#!/usr/bin/php -q
+<?php
+
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'abc1234');
+define('DB_SERVER', 'localhost');
+define('DB_DATABASE', 'log');
+
+$extensions = array("1111", "2222","3333");
+
+$conn = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die("Can't connect to database");
+mysql_select_db(DB_DATABASE) or die("Can't select database");
+mysql_query("SET NAMES UTF8");
+
+function insertstatus($extension, $host, $port, $status) {
+    global $conn;
+    $strSQL = "INSERT INTO `logstatus_users`( `date`, `extension`, `host`, `port`, `status`) VALUES (NOW(),'{$extension}','{$host}','{$port}','{$status}')";
+    mysql_query($strSQL, $conn);
+}
+
+foreach ($extensions as $extension) {
+    $resultexec = shell_exec("/usr/sbin/asterisk -rx 'sip show peers' | grep {$extension}");
+    $resultreplace =  preg_replace("/([ ]{2,})/","|",$resultexec);
+    $results = explode("|", $resultreplace);
+    insertstatus($extension, $results[1], $results[5], $results[6]);
+}
+?>
