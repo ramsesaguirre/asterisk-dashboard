e73712dbc1eb8e1180b516f4f254851acc88e651
Sirachat <sirachat.sir@gmail.com>
2014-07-02 10:41:09 +0700

    Update 4: แก้บั๊ค จัดหน้าโค๊ด
    แก้บั๊คเกี่ยวกับไฟล์ที่เหลือ กับ จัดหน้าให้เรียบร้อย น่าจะยัดลง API
    ได้เลย (มั้ง)

--- abandoncall.php
+++ abandoncall.php
@@ -8,26 +8,35 @@ function dateInputEnd($dateEnd){
 	return $dateEnd;
 }
 
-$date1 = dateInputStart("2014-06-26");
-$date2 = dateInputEnd("2014-06-28");
+$date1 = dateInputStart("2014-07-01");
+$date2 = dateInputEnd("2014-07-01");
+
+//check condition to connect mysql (if $date1 and $date 2 are same, sql could be find only $date1)
 include "dbconnection.php";
 if($date1 == $date2){
-	echo "Check abandon called from date : ".$date1."</p>";
 	$sql = " SELECT `calldate`,`dstchannel`,`src`,`dst`,`duration` FROM cdr WHERE `calldate` LIKE '$date1"."%' AND `dstchannel` = '' AND `lastapp` = 'Queue' ";
+	echo $sql."</p>";
+	echo "Check abandon called from date : ".$date1."</p>";
 }
 else{
+	if($date1 == '0' || $date2 == '0'){
+		$date1 = $date2;
+	}
 	echo "Check abandon called between date : ".$date1." to ".$date2."</p>";
 	$sql = " SELECT `calldate`,`dstchannel`,`src`,`dst`,`duration` FROM cdr WHERE `calldate` BETWEEN '$date1"."%' AND '$date2"."%' AND `dstchannel` = '' AND `lastapp` = 'Queue' ";
+
 }
 
 $query = mysql_query($sql);
 $sumRows = 0;
 while($row = mysql_fetch_assoc($query))
 	{
+		//Output variable -> '$calldate' for showing date of abandon called and 
+		//-> '$callSource' for showing number of abandoncall
 		$sumRows += $row['duration'];
 		$callDate = $row['calldate'];
 		$callSource = $row['src'];
-		echo $callDate." -> ".$callSource.'</br>';
+		echo 'call date : '.$callDate." number -> ".$callSource.'</br>';
 
 	}
 $count = mysql_num_rows($query);
--- totalcalled.php
+++ totalcalled.php
@@ -1,5 +1,5 @@
 <?PHP
-
+//function 'timeDifferent' can be return different of time. Function process are convert time to unix and find different time.
 function timeDifferent($firstTimeInput,$lastTimeInput)
 {
 	$firstTimeInput = strtotime($firstTimeInput);
@@ -7,16 +7,6 @@ function timeDifferent($firstTimeInput,$lastTimeInput)
 	$timeDifferent = $lastTimeInput - $firstTimeInput;
 	return $timeDifferent;
 }
-
-// function averageTime($values)
-// {
-// 	$average=0;
-// 	foreach ($values as $value) 
-// 	{
-// 		$average+=$value;
-// 	}
-// 	return $average/count($values);
-// }
 
 function inputDate($dateInput)
 {
@@ -26,15 +16,17 @@ function inputDate($dateInput)
 }
 
 include "dbconnection.php";
-$date = inputDate('2014-06-26');
-echo "Find Call log date : ".$date."</p>";
+$date = inputDate('2014-07-01');
+
 /* USING 'CEL' DATABASE TO ANALYSE TOTAL CALL OF USER TO OPERATOR  */
-$sqlCMD = "select `eventtime`,`eventtype`,`cid_num`,`linkedid` from `cel` where (`eventtime` like '$date"."%') and (`eventtype` = 'BRIDGE_START' or `eventtype` = 'BRIDGE_END') GROUP by `eventtime` order by `eventtime` ASC ";
+$sqlCMD = "select `eventtime`,`eventtype`,`cid_num`,`linkedid` from `cel` where (`eventtime` like '$date"."%') and (`eventtype` = 'BRIDGE_START' or `eventtype` = 'BRIDGE_END') GROUP by `eventtime` order by `linkedid`,`eventtype` DESC ";
 $query = mysql_query($sqlCMD);
 $countRows = mysql_num_rows($query);
-$count=0;
-$sumVar = 0;
-$tmp =null;
+
+// OPERATOR COUNT
+$count=0; //count variable for number of data
+$sumVar = 0; //summary of data
+$tmp =null; //temp files 
 while ($rowstart = mysql_fetch_assoc($query) and $rowend = mysql_fetch_assoc($query))
 {
 	if ($rowstart['linkedid']==$rowend['linkedid'])
@@ -46,12 +38,9 @@ while ($rowstart = mysql_fetch_assoc($query) and $rowend = mysql_fetch_assoc($qu
 	}
 }
 $averageTimePerDay = $sumVar / $count;
+
+// SHOW ON PAGES
+echo "Find Call log date : ".$date."</p>";
 echo "<p>average time per day : ".gmdate("i:s",$averageTimePerDay)." Minute";
-// echo averageTime($tmp);
-// $testFirstTime = "2014-06-26 13:05:56";
-// $testLastTime = "2014-06-26 13:06:11";
-// echo strtotime($testFirstTime).'<br>';
-// echo strtotime($testLastTime);
-// echo timeDifferent($testFirstTime,$testLastTime);
 
 ?>
