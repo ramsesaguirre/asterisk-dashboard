aada8acf6b77b82c94403ced15c742597e9219da
zinzoz <mickyo_street@hotmail.com>
2014-07-04 13:59:28 +0700

    no message

--- num-operator2.php
+++ num-operator2.php
@@ -10,34 +10,35 @@ include "dbconnection2log.php";
 
 
 
-$sqlCMD = "SELECT `datetime`,`operator_id`,`status` FROM `log_operator`    order by `datetime` desc" ;
+$sqlCMD = "SELECT `date`,`operator_id`,`status` FROM `log_operator` order by `date` desc" ;
 $query = mysql_query($sqlCMD);
 $countRows = mysql_num_rows($query);
 
 $o1=$o2=$o3='2100-07-03 11:14:31';
 
-$oper1='1111';
-$oper2='2222';
-$oper3='3333';
+$oper1='OP-1111';
+$oper2='OP-2222';
+$oper3='OP-3333';
+$count=0;
 
 while ($rowstart = mysql_fetch_assoc($query) )
 {  
     if($rowstart['operator_id']==$oper1)    
 	   { 
-	   	  if ($o1> $rowstart['datetime'])
+	   	  if ($o1> $rowstart['date'])
 	     	{
 		
-				echo "operator : ".$rowstart['operator_id']." status: On  Time:".$rowstart['datetime'].'<br>';
+				echo "operator : ".$rowstart['operator_id']." status: On  Time:".$rowstart['date'].'<br>';
 		
 	     	}
 	   		 $o1='1000-07-03 11:14:31';
 	   }
      elseif ($rowstart['operator_id']==$oper2) 
      	{
-     	   if ($o2> $rowstart['datetime'])
+     	   if ($o2> $rowstart['date'])
 	    	 {
 		
-				echo "operator : ".$rowstart['operator_id']." status: On  Time:".$rowstart['datetime'].'<br>';
+				echo "operator : ".$rowstart['operator_id']." status: On  Time:".$rowstart['date'].'<br>';
 		
 	    	 }
 	 		$o2='1000-07-03 11:14:31';
@@ -45,17 +46,22 @@ while ($rowstart = mysql_fetch_assoc($query) )
     	 }
     elseif ($rowstart['operator_id']==$oper3) 
      	{
-     	   if ($o3> $rowstart['datetime'])
+     	   if ($o3> $rowstart['date'])
 	    	 {
 		
-				echo "operator : ".$rowstart['operator_id']." status: On  Time:".$rowstart['datetime'].'<br>';
+				echo "operator : ".$rowstart['operator_id']." status: On  Time:".$rowstart['date'].'<br>';
 		
 	    	 }
 	 		$o3='1000-07-03 11:14:31';
      	# code...
     	 }
+    	 
+     $count++;
+     if ($count==30)
+     	break;
 
 
+ 
 }
 
 // SHOW ON PAGES
