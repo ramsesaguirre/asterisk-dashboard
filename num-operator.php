02bde78596770f84f3788f9f285581dee98dd9e5
Sirachat <sirachat.sir@gmail.com>
2014-07-04 11:16:51 +0700

    Version 2 Update 3 : Critical Update
    critical update , fixed bug
    - num-operator.php —> fixed bug time synchronous w/ server

--- num-operator.php
+++ num-operator.php
@@ -3,30 +3,56 @@ define('DB_USER','root');
 define('DP_PASS','abc1234');
 define('DB_SERV','localhost');
 define('DB_NAME','log');
+define('DB_TABLE','log_operator');
+date_default_timezone_set('Asia/Bangkok');
+$conn_db = mysql_connect(DB_SERV,DB_USER,DP_PASS) or die ("Can't Connect to database");
+mysql_select_db(DB_NAME);
 
 $operator_name = array("OP-1111","OP-2222","OP-3333");
 $operator_number = "89000";
 
-$strToFind = "Not in use";
-$strAbsent = "Unavailable";
+$strNotInUse = "Not in use";  # AVALIABLE
+$strUnavailable = "Unavailable";  # OFFLINE
+$strRinging = "Ringing"; # RINGING
+$strInUse = "In use"; # BUSY
 
+$tmpTime = NULL;
+function insertOperatorToSQL($operator_id, $serv_time, $status) {
+	global $conn_db;
+	$insert = " INSERT INTO `log_operator`( `date`, `operator_id`, `status`) 
+	VALUES ('{$serv_time}','{$operator_id}','{$status}')";
+	mysql_query($insert,$conn_db);
+}
+
+function findByDate($time,$condition){
+	if($condition==NULL || $condition == '') $condition = '=';
+	// echo 'SELECT * FROM `log_operator` WHERE `date` {$condition} {$time}';
+	$query = mysql_query('SELECT * FROM `log_operator` WHERE `date` $condition $time');
+	echo $query;
+	while ($row = mysql_fetch_assoc($query)){
+		echo $row['operator_id'];
+	}
+}
+$serv_time = $tmpTime = date('Y-m-d h:m:s');
 foreach ($operator_name as $operator) {
 	$exec = shell_exec("/usr/sbin/asterisk -rx 'queue show {$operator_number}' | grep {$operator}");
+	// echo $exec;
 	# find position string
-	$posStr = strpos($exec, $strToFind);
-	$posStr2 = strpos($exec, $strAbsent);
-
+	$posStr = strpos($exec, $strNotInUse);
+	$posStr2 = strpos($exec, $strUnavailable);
 	if($posStr == true || $posStr2 == true){
 		if($posStr2 == true){
-			echo "Operator ({$operator}) not online !!".'<p>';
-			break;
+			echo "Operator ({$operator}) not online !! --> ";
 		}
-		echo "Operator ({$operator}) avaliable !!".'<p>';
+		else echo "Operator ({$operator}) avaliable !! --> ";
 	}else{
-		echo "Operator ({$operator}) busy !!".'<p>';
+		echo "Operator ({$operator}) busy !! --> ";
 	}
+	$tmp = ($posStr)?($strNotInUse):(($posStr2)?$strUnavailable:$strInUse);
+	echo $tmp.' </br>';
 
-
+	insertOperatorToSQL($operator,$serv_time,$tmp);
+	
 
 	// if($posStr == false)
 	// if($posStr == false || $posStr2 == ){
@@ -35,5 +61,7 @@ foreach ($operator_name as $operator) {
 	// 	echo "Operator ({$operator}) avaliable!!".'<p>';
 	// }
 }
+echo $tmpTime;
+echo findByDate($tmpTime,'>=');
 
 ?>
